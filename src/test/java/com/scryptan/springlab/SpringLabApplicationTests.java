970cc211573b9eea4d3477cc05895171b45a96cf
scryptan <scyptan@gmail.com>
2023-02-22 20:03:28 +0500

    create simple project template

---
+++ src/test/java/com/scryptan/springlab/SpringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scryptan.springlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
