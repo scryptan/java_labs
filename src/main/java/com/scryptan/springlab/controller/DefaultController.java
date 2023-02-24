0113203db55358abff3ba5637d4e4736d200b563
scryptan <scyptan@gmail.com>
2023-02-24 21:14:53 +0500

    create controller with default view

--- src/main/java/com/scryptan/springlab/controller/DefaultController.java
+++ src/main/java/com/scryptan/springlab/controller/DefaultController.java
@@ -1,51 +1,25 @@
 package com.scryptan.springlab.controller;
 
-import com.scryptan.springlab.model.Request;
-import com.scryptan.springlab.model.Response;
-import com.scryptan.springlab.service.IModifyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j()
-@RestController
+@Controller
 public class DefaultController {
 
-    private final IModifyService modifyUid;
-    private final IModifyService modifyCode;
-
     @Autowired
-    public DefaultController(@Qualifier("ModifyUid") IModifyService modifyUid, @Qualifier("ModifyCode") IModifyService modifyCode) {
-        this.modifyUid = modifyUid;
-
-        this.modifyCode = modifyCode;
+    public DefaultController() {
     }
 
-    @PostMapping(value = "/feedback")
-    public ResponseEntity<Response> feedback(@RequestBody Request request) {
-        Response response = Response
-                .builder()
-                .uid(request.getUid())
-                .operationUid(request.getOperationUid())
-                .systemTime(request.getSystemTime())
-                .code(HttpStatus.OK.getReasonPhrase())
-                .errorCode("")
-                .errorMessage("")
-                .build();
-
-        log.info(String.format("Response before modify %s", response));
-
-        modifyUid.modify(response);
-        modifyCode.modify(response);
-
-        log.warn(String.format("Response after modify %s", response));
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    @GetMapping(value = "/hello")
+    public String feedback(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+                           Model model) {
+        model.addAttribute("name", name);
+        return "my-first-page";
     }
 
 }
